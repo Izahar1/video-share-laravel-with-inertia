e736ace1dcd6cc2b1b178a089ff338b2c991e41e
Izahar1 <iza10102002har@gmail.com>
2023-12-07 01:01:26 +0530

    Add Home page Create video and Video Index Controller

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Http\Controllers\CreateVideoController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\VideoIndexController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use App\Http\Controllers;
 use App\Http\Controllers\HomeController;
 
 /*
@@ -20,11 +21,15 @@ use App\Http\Controllers\HomeController;
 
 Route::get('/', HomeController::class)->name('home');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
+
+    Route::get('/videos',[VideoIndexController::class])->name('video.index');
+    Route::get('/videos/create', [CreateVideoController::class])->name('videos.crate');
+
+
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
